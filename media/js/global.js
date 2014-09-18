9f3726bb4c202dfd23ef47c43e43f23e5b2e98fe
Sakto <daniel@danwebs.dk>
2014-09-18 18:59:48 +0200

    remove fancybox lib

--- media/js/global.js
+++ media/js/global.js
@@ -1,15 +1,1 @@
-$(document).ready(function(){
-	
-	/**
-	 * 
-	 */
-	$('.fancybox').fancybox();
-	
-});
 
-function scrollToElement(element)
-{
-	$('body, html').animate({
-		scrollTop:$(element).offset().top
-	});
-}
