5432fd187ff3a841e97e12cc7f5794c5e303c63c
Sakto <daniel@danwebs.dk>
2014-12-01 14:45:10 +0100

    More layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
 module.exports = function(grunt){
 	grunt.initConfig({
 		pkg:grunt.file.readJSON('package.json'),
+		autoprefixer: {
+			dist: {
+				files: {
+					'media/css/style.css':'media/css/style.css'
+				}
+			}
+		},
 		sass:{
 			dist:{
 				options:{
@@ -14,21 +21,13 @@ module.exports = function(grunt){
 		watch: {
 			css: {
 				files: ['media/sass/*.scss', 'media/sass/*/*.scss', 'media/sass/*/*/*.scss'],
-				tasks: ['sass']
-			}
-		},
-		cssmin: {
-			css:{
-				files:{
-					'media/css/style.min.css':'media/css/style.css'
-				}
+				tasks: ['sass', 'autoprefixer']
 			}
 		}
 	});
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.registerTask('minifycss', ['cssmin']);
 	grunt.registerTask('default', ['sass']);
 	//grunt.registerTask('default', ['watch']);
 };
